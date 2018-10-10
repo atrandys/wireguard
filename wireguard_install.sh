bb38556b8d71311d464462342019b7e0b4d22cfa
yobabyshark <37266675+yobabyshark@users.noreply.github.com>
2018-10-10 22:28:09 +0800

    Update wireguard_install.sh

--- wireguard_install.sh
+++ wireguard_install.sh
@@ -10,22 +10,23 @@ echo "仅支持centos7"
 exit
 fi
 
-#更新epel
-sudo yum -y install epel-release
-sed -i "0,/enabled=0/s//enabled=1/" /etc/yum.repos.d/epel.repo
+
 
 #更新内核
 update_kernel(){
+
+    sudo yum -y install epel-release
+    sed -i "0,/enabled=0/s//enabled=1/" /etc/yum.repos.d/epel.repo
+    yum remove -y kernel-devel
     rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
     rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm
     yum --disablerepo="*" --enablerepo="elrepo-kernel" list available
     yum -y --enablerepo=elrepo-kernel install kernel-ml
     sed -i "s/GRUB_DEFAULT=saved/GRUB_DEFAULT=0/" /etc/default/grub
     grub2-mkconfig -o /boot/grub2/grub.cfg
-    yum remove -y kernel-devel
-    #wget http://elrepo.org/linux/kernel/el7/x86_64/RPMS/kernel-ml-devel-4.18.12-1.el7.elrepo.x86_64.rpm
-    #rpm -ivh kernel-ml-devel-4.18.12-1.el7.elrepo.x86_64.rpm
-    #yum --enablerepo=elrepo-kernel install kernel-ml-devel
+    wget http://elrepo.org/linux/kernel/el7/x86_64/RPMS/kernel-ml-devel-4.18.12-1.el7.elrepo.x86_64.rpm
+    rpm -ivh kernel-ml-devel-4.18.12-1.el7.elrepo.x86_64.rpm
+    yum --enablerepo=elrepo-kernel install kernel-ml-devel
     read -p "需要重启VPS，再次执行脚本选择安装wireguard，是否现在重启 ? [Y/n] :" yn
 	[ -z "${yn}" ] && yn="y"
 	if [[ $yn == [Yy] ]]; then
