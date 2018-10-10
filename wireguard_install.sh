f94445ae5ee7a2dac48efc41d083ad2c4f57fac7
yobabyshark <37266675+yobabyshark@users.noreply.github.com>
2018-10-10 21:19:40 +0800

    Update wireguard_install.sh

--- wireguard_install.sh
+++ wireguard_install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#颜色
+Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 #判断系统
 if [ ! -e '/etc/redhat-release' ]; then
 echo "仅支持centos7"
@@ -10,7 +12,9 @@ echo "仅支持centos7"
 exit
 fi
 
-
+#更新epel
+sudo yum -y install epel-release
+sed -i "0,/enabled=0/s//enabled=1/" /etc/yum.repos.d/epel.repo
 
 #更新内核
 update_kernel(){
@@ -25,6 +29,12 @@ update_kernel(){
     #wget http://elrepo.org/linux/kernel/el7/x86_64/RPMS/kernel-ml-devel-4.18.12-1.el7.elrepo.x86_64.rpm
     #rpm -ivh kernel-ml-devel-4.18.12-1.el7.elrepo.x86_64.rpm
     #yum --enablerepo=elrepo-kernel install kernel-ml-devel
+    read -p "需要重启VPS，再次执行脚本选择安装wireguard，是否现在重启 ? [Y/n] :" yn
+	[ -z "${yn}" ] && yn="y"
+	if [[ $yn == [Yy] ]]; then
+		echo -e "${Info} VPS 重启中..."
+		reboot
+	fi
 }
 
 #生成随机端口
@@ -35,11 +45,9 @@ rand(){
     echo $(($num%$max+$min))  
 }
 
-#centos安装wireguard，官方命令
+#centos7安装wireguard
 wireguard_install(){
     sudo curl -Lo /etc/yum.repos.d/wireguard.repo https://copr.fedorainfracloud.org/coprs/jdoss/wireguard/repo/epel-7/jdoss-wireguard-epel-7.repo
-    sudo yum -y install epel-release
-    sed -i "0,/enabled=0/s//enabled=1/" /etc/yum.repos.d/epel.repo
     sudo yum install -y dkms gcc-c++ gcc-gfortran glibc-headers glibc-devel libquadmath-devel libtool systemtap systemtap-devel
     sudo yum -y install wireguard-dkms wireguard-tools
     mkdir /etc/wireguard
@@ -53,7 +61,8 @@ wireguard_install(){
     serverip=$(curl icanhazip.com)
     port=$(rand 10000 60000)
     chmod 777 -R /etc/wireguard
-    vi /etc/wireguard/wg0.conf
+    echo 1 > /proc/sys/net/ipv4/ip_forward
+    echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
     cat > /etc/wireguard/wg0.conf <<-EOF
     [Interface]
     PrivateKey = $s1
@@ -83,7 +92,32 @@ wireguard_install(){
     systemctl enable wg-quick@wg0
 }
 
-
+#开始菜单
+start_menu(){
+clear
+ ${Green_font_prefix}1.${Font_color_suffix} 升级系统内核
+ ${Green_font_prefix}2.${Font_color_suffix} 安装wireguard
+————————————————————————————————" && echo
+echo
+read -p " 请输入数字:" num
+case "$num" in
+	1)
+	update_kernel
+	;;
+	2)
+	wireguard_install
+	;;
+	3)
+	exit 1
+	;;
+	*)
+	clear
+	echo -e "${Error}:请输入正确数字"
+	sleep 5s
+	start_menu
+	;;
+esac
+}
 
 
 
