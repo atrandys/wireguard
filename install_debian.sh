8b1b3cbb7834acad02f549c1eb2406bcc4267380
YuanSuyi <yuansuyi@hzdusun.com>
2018-11-07 16:14:33 +0800

    typo

--- install_debian.sh
+++ install_debian.sh
@@ -76,7 +76,7 @@ configure_wireguard()
 
 	mv /etc/wireguard/wg0.conf /etc/wireguard/wg0.conf.bak  2> /dev/null
 
-	ip = $SUBNET.2
+	ip=$SUBNET.2
 	cat > /etc/wireguard/wg0.conf <<-EOF
 	[Interface]
 	PrivateKey = $SERVER_PRIV
