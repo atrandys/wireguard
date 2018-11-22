4b65b8abd34ea1c7f18c6cae51950d5db83e8b87
yobabyshark <37266675+yobabyshark@users.noreply.github.com>
2018-11-22 11:12:10 +0800

    Update wireguard_install.sh

--- wireguard_install.sh
+++ wireguard_install.sh
@@ -43,6 +43,11 @@ rand(){
     echo $(($num%$max+$min))  
 }
 
+wireguard_update(){
+    yum update -y wireguard-dkms wireguard-tools
+    echo "更新完成"
+}
+
 config_client(){
 cat > /etc/wireguard/client.conf <<-EOF
 [Interface]
@@ -124,7 +129,8 @@ start_menu(){
     echo "========================="
     echo "1. 升级系统内核"
     echo "2. 安装wireguard"
-    echo "3. 退出脚本"
+    echo "3. 升级wireguard"
+    echo "4. 退出脚本"
     echo
     read -p "请输入数字:" num
     case "$num" in
@@ -135,6 +141,9 @@ start_menu(){
 	wireguard_install
 	;;
 	3)
+	wireguard_update
+	;;
+	4)
 	exit 1
 	;;
 	*)
