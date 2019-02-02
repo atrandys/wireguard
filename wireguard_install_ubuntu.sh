0c55a1c682362571107190561d8c49748435eb59
atrandys <37266675+atrandys@users.noreply.github.com>
2019-02-02 13:24:04 +0800

    Update wireguard_install_ubuntu.sh

--- wireguard_install_ubuntu.sh
+++ wireguard_install_ubuntu.sh
@@ -110,8 +110,8 @@ wireguard_remove(){
 start_menu(){
     clear
     echo -e "\033[43;42m ====================================\033[0m"
-    echo -e "\033[43;42m 介绍：wireguard+udpspeeder+udp2raw  \033[0m"
-    echo -e "\033[43;42m 系统：Ubuntu                      \033[0m"
+    echo -e "\033[43;42m 介绍：wireguard一键脚本              \033[0m"
+    echo -e "\033[43;42m 系统：Ubuntu                        \033[0m"
     echo -e "\033[43;42m 作者：atrandys                      \033[0m"
     echo -e "\033[43;42m 网站：www.atrandys.com              \033[0m"
     echo -e "\033[43;42m Youtube：atrandys                   \033[0m"
