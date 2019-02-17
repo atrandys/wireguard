b28e0ce9e6e6d13d9a9bc2dc093559a691fb8d73
atrandys <37266675+atrandys@users.noreply.github.com>
2019-02-17 19:31:27 +0800

    Update wireguard_install_ubuntu.sh

--- wireguard_install_ubuntu.sh
+++ wireguard_install_ubuntu.sh
@@ -106,6 +106,28 @@ wireguard_remove(){
     sudo rm -rf /etc/wireguard
 
 }
+
+add_user(){
+    echo -e "\033[37;41m给新用户起个名字，不能和已有用户重复\033[0m"
+    read -p "请输入用户名：" newname
+    cd /etc/wireguard/
+    cp client.conf $newname.conf
+    wg genkey | tee temprikey | wg pubkey > tempubkey
+    ipnum=$(grep Allowed /etc/wireguard/wg0.conf | tail -1 | awk -F '[ ./]' '{print $6}')
+    newnum=$((10#${ipnum}+1))
+    sed -i 's%^PrivateKey.*$%'"PrivateKey = $(cat temprikey)"'%' $newname.conf
+    sed -i 's%^Address.*$%'"Address = 10.0.0.$newnum\/24"'%' $newname.conf
+
+cat >> /etc/wireguard/wg0.conf <<-EOF
+[Peer]
+PublicKey = $(cat tempubkey)
+AllowedIPs = 10.0.0.$newnum/32
+EOF
+    wg set wg0 peer $(cat tempubkey) allowed-ips 10.0.0.$newnum/32
+    echo -e "\033[37;41m添加完成，文件：/etc/wireguard/$newname.conf\033[0m"
+    rm -f temprikey tempubkey
+}
+
 #开始菜单
 start_menu(){
     clear
@@ -120,6 +142,7 @@ start_menu(){
     echo -e "\033[0;33m 1. 安装wireguard\033[0m"
     echo -e "\033[0;33m 2. 查看客户端二维码\033[0m"
     echo -e "\033[0;31m 3. 删除wireguard\033[0m"
+    echo -e "\033[0;33m 4. 增加用户\033[0m"
     echo -e " 0. 退出脚本"
     echo
     read -p "请输入数字:" num
@@ -132,6 +155,9 @@ start_menu(){
     echo "${content}" | qrencode -o - -t UTF8
     ;;
     3)
+    wireguard_remove
+    ;;
+    4)
     wireguard_remove
     ;;
     0)
