d2253716eeb131ab5f0c5b6ff31610e7bc510df9
atrandys <37266675+atrandys@users.noreply.github.com>
2020-01-22 15:02:32 +0800

    Update wg_mult.sh

--- wg_mult.sh
+++ wg_mult.sh
@@ -113,26 +113,112 @@ function config_wg(){
     eth=$(ls /sys/class/net| awk 'NR==1&&/^e/{print $1}')
     chmod 777 -R /etc/wireguard
 
-
 cat > /etc/wireguard/wg0.conf <<-EOF
 [Interface]
 PrivateKey = $s1
-Address = 10.0.0.1/24 
-PostUp   = echo 1 > /proc/sys/net/ipv4/ip_forward; iptables -A FORWARD -i wg0 -j ACCEPT; iptables -A FORWARD -o wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o $eth -j MASQUERADE
+Address = 10.77.0.1/24 
+PostUp   = iptables -A FORWARD -i wg0 -j ACCEPT; iptables -A FORWARD -o wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o $eth -j MASQUERADE
 PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -D FORWARD -o wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o $eth -j MASQUERADE
 ListenPort = $port
 DNS = 8.8.8.8
 MTU = 1420
 [Peer]
 PublicKey = $c2
-AllowedIPs = 10.0.0.2/32
+AllowedIPs = 10.77.0.2/32
 EOF
 
-    config_client
+cat > /etc/wireguard/client.conf <<-EOF
+[Interface]
+PrivateKey = $c1
+Address = 10.77.0.2/24 
+DNS = 8.8.8.8
+MTU = 1420
+[Peer]
+PublicKey = $s2
+Endpoint = $serverip:$port
+AllowedIPs = 0.0.0.0/0, ::0/0
+PersistentKeepalive = 25
+EOF
     wg-quick up wg0
     systemctl enable wg-quick@wg0
     content=$(cat /etc/wireguard/client.conf)
-    echo "电脑端请下载client.conf，手机端可直接使用软件扫码"
-    echo "${content}" | qrencode -o - -t UTF8
+    green "电脑端请下载/etc/wireguard/client.conf文件，手机端可直接使用软件扫码"
+    green "${content}" | qrencode -o - -t UTF8
 
 }
+
+function add_user(){
+
+    green "=================================="
+    green "给新用户起个名字，不能和已有用户重复"
+    green "=================================="
+    read -p "请输入用户名：" newname
+    cd /etc/wireguard/
+    if [ ! -f "/etc/wireguard/$newname" ]; then
+        cp client.conf $newname.conf
+    	wg genkey | tee temprikey | wg pubkey > tempubkey
+    	ipnum=$(grep Allowed /etc/wireguard/wg0.conf | tail -1 | awk -F '[ ./]' '{print $6}')
+    	newnum=$((10#${ipnum}+1))
+    	sed -i 's%^PrivateKey.*$%'"PrivateKey = $(cat temprikey)"'%' $newname.conf
+    	sed -i 's%^Address.*$%'"Address = 10.77.0.$newnum\/24"'%' $newname.conf
+    else
+    	red "======================"
+	red "用户名已存在，请更换名称"
+	red "======================"
+    fi
+cat >> /etc/wireguard/wg0.conf <<-EOF
+[Peer]
+PublicKey = $(cat tempubkey)
+AllowedIPs = 10.77.0.$newnum/32
+EOF
+    wg set wg0 peer $(cat tempubkey) allowed-ips 10.77.0.$newnum/32
+    green "============================================="
+    green "添加完成，文件：/etc/wireguard/$newname.conf"
+    green "============================================="
+    rm -f temprikey tempubkey
+}
+
+function start_menu(){
+    clear
+    green "===================================="
+    green " Info   : For Centos7+/Ubuntu/Debian"
+    green " Author : atrandys"
+    green " Website: www.atrandys.com"
+    green " YouTube: Randy's 堡垒"
+    green "===================================="
+    green "1. Install wireguard"
+    red "2. Remove wireguard"
+    green "3. Show client QRcode"
+    green "4. Add user"
+    red "0. Exit"
+    echo
+    read -p "Please enter a number:" num
+    case "$num" in
+    	1)
+	check_selinux
+	install_wg
+	config_wg
+	;;
+	2)
+	remove_wg
+	;;
+	3)
+	content=$(cat /etc/wireguard/client.conf)
+    	echo "${content}" | qrencode -o - -t UTF8
+	;;
+	4)
+	add_user
+	;;
+	0)
+	exit 1
+	;;
+	*)
+	clear
+	red "Please enter the correct number!"
+	sleep 1s
+	start_menu
+	;;
+    esac
+}
+
+start_menu
