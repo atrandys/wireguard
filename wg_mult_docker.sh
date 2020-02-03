0cd60868471c3a86cbfa5dba66cde11eedc0e8b4
atrandys <37266675+atrandys@users.noreply.github.com>
2020-02-03 22:31:41 +0800

    Update wg_mult_docker.sh

--- wg_mult_docker.sh
+++ wg_mult_docker.sh
@@ -51,12 +51,30 @@ check_release(){
 docker_install(){
     check_release
     if [ "$RELEASE" == "centos" ]; then
-    
+        yum remove -y docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
+	yum install -y yum-utils device-mapper-persistent-data lvm2
+	yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+	yum install -y docker-ce docker-ce-cli containerd.io
     elif [ "$RELEASE" == "ubuntu" ]; then
-    
+        apt-get remove -y docker docker-engine docker.io containerd runc
+	apt-get update
+	apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+	add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+	apt-get update
+	apt-get install -y docker-ce docker-ce-cli containerd.io
     elif [ "$RELEASE" == "debian" ]; then
-    
+        apt-get remove -y docker docker-engine docker.io containerd runc
+	apt-get update
+	apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+	add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+	apt-get update
+	apt-get install -y docker-ce docker-ce-cli containerd.io
     else
-    
+        red "======================================="
+    	red "Does not supported Current OS"$RELEASE
+	red "======================================="
+	exit 1
     fi
 }
