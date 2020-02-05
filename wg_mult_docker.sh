e55f8a5c46a1ed0195110849477b4c685c9a1fc6
atrandys <37266675+atrandys@users.noreply.github.com>
2020-02-05 18:39:42 +0800

    Update wg_mult_docker.sh

--- wg_mult_docker.sh
+++ wg_mult_docker.sh
@@ -10,6 +10,13 @@ red(){
     echo -e "\033[31m\033[01m$1\033[0m"
 }
 
+rand(){
+    min=$1
+    max=$(($2-$min+1))
+    num=$(cat /dev/urandom | head -n 10 | cksum | awk -F ' ' '{print $1}')
+    echo $(($num%$max+$min))  
+}
+
 check_selinux(){
     CHECK=$(grep SELINUX= /etc/selinux/config | grep -v "#")
     if [ "$CHECK" == "SELINUX=enforcing" ]; then
@@ -96,8 +103,46 @@ docker_install(){
 wg_install(){
     docker pull atrandys/wireguard
     mkdir /etc/wireguard
-    docker run -itd -P --name wireguard -v /etc/wireguard:/etc/wireguard atrandys/wireguard /bin/sh
-    
+    cd /etc/wireguard
+    docker run -itd -P --restart=always --name wireguard -v /etc/wireguard:/etc/wireguard atrandys/wireguard /bin/sh
+    docker exec -it wireguard /bin/sh "cd /etc/wireguard;wg genkey | tee sprivatekey | wg pubkey > spublickey;wg genkey | tee cprivatekey | wg pubkey > cpublickey"
+    s1=$(cat sprivatekey)
+    s2=$(cat spublickey)
+    c1=$(cat cprivatekey)
+    c2=$(cat cpublickey)
+    serverip=$(curl ipv4.icanhazip.com)
+    port=$(rand 10000 60000)
+    eth=$(ls /sys/class/net| awk 'NR==1&&/^e/{print $1}')
+    #chmod 777 -R /etc/wireguard
+
+cat > /etc/wireguard/wg0.conf <<-EOF
+[Interface]
+PrivateKey = $s1
+Address = 10.77.0.1/24 
+PostUp   = iptables -t nat -A POSTROUTING -o $eth -j MASQUERADE
+PostDown = iptables -t nat -D POSTROUTING -o $eth -j MASQUERADE
+ListenPort = $port
+DNS = 8.8.8.8
+MTU = 1420
+[Peer]
+PublicKey = $c2
+AllowedIPs = 10.77.0.2/32
+EOF
+
+cat > /etc/wireguard/client.conf <<-EOF
+[Interface]
+PrivateKey = $c1
+Address = 10.77.0.2/24 
+DNS = 8.8.8.8
+MTU = 1420
+[Peer]
+PublicKey = $s2
+Endpoint = $serverip:$port
+AllowedIPs = 0.0.0.0/0, ::0/0
+PersistentKeepalive = 25
+EOF
+    docker exec -it wireguard /bin/sh -c "wg-quick up wg0"
 }
+
 docker_install
 wg_install
