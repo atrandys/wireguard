eb79260fd8016a9b8a72f4e5b632620100ec2204
atrandys <37266675+atrandys@users.noreply.github.com>
2020-02-05 20:08:42 +0800

    Update wg_mult_docker.sh

--- wg_mult_docker.sh
+++ wg_mult_docker.sh
@@ -142,7 +142,89 @@ AllowedIPs = 0.0.0.0/0, ::0/0
 PersistentKeepalive = 25
 EOF
     docker exec -it wireguard /bin/sh -c "wg-quick up wg0"
+    green "========================================================="
+    green "wireguard安装完成，请复制以下红色内容复制，保存为client.conf"
+    green "在wireguard客户端导入client.conf即可连接"
+    red `cat /etc/wireguard/client.conf`
 }
 
-docker_install
-wg_install
+user_add(){
+    green "=================================="
+    green "给新用户起个名字，不能和已有用户重复"
+    green "=================================="
+    read -p "请输入用户名：" newname
+    cd /etc/wireguard/
+    if [ ! -f "/etc/wireguard/$newname.conf" ]; then
+        cp client.conf $newname.conf
+    	docker exec -it wireguard /bin/sh -c "wg genkey | tee temprikey | wg pubkey > tempubkey"
+    	ipnum=$(grep Allowed /etc/wireguard/wg0.conf | tail -1 | awk -F '[ ./]' '{print $6}')
+    	newnum=$((10#${ipnum}+1))
+    	sed -i 's%^PrivateKey.*$%'"PrivateKey = $(cat temprikey)"'%' $newname.conf
+    	sed -i 's%^Address.*$%'"Address = 10.77.0.$newnum\/24"'%' $newname.conf
+	cat >> /etc/wireguard/wg0.conf <<-EOF
+[Peer]
+PublicKey = $(cat tempubkey)
+AllowedIPs = 10.77.0.$newnum/32
+EOF
+    	docker exec -it wireguard /bin/sh -c "wg set wg0 peer $(cat tempubkey) allowed-ips 10.77.0.$newnum/32"
+    	green "============================================="
+    	green "添加完成，文件：/etc/wireguard/$newname.conf"
+    	green "============================================="
+    	rm -f temprikey tempubkey
+    else
+    	red "======================"
+	red "用户名已存在，请更换名称"
+	red "======================"
+    fi
+}
+
+wg_remove(){
+    if [ -d "/etc/wireguard" ]; then
+        docker update --restart=no wireguard
+	docker stop wireguard
+	docker rm wireguard
+	rm -rf /etc/wireguard/
+	green "wireguard docker容器已删除"
+	green "wireguard配置文件已删除"
+    else
+        red "你似乎没有安装wireguard"
+}
+
+start_menu(){
+    clear
+    green "=========================================="
+    green " Info   : For Centos7+/Ubuntu16+/Debian9+"
+    green " Author : atrandys"
+    green " Website: www.atrandys.com"
+    green " YouTube: Randy's 堡垒"
+    green "=========================================="
+    green "1. Install wireguard"
+    red "2. Remove wireguard"
+    green "3. Add user"
+    red "0. Exit"
+    echo
+    read -p "Please enter a number:" num
+    case "$num" in
+    	1)
+	docker_install
+        wg_install
+	;;
+	2)
+	wg_remove
+	;;
+	3)
+	user_add
+	;;
+	0)
+	exit 1
+	;;
+	*)
+	clear
+	red "Please enter the correct number!"
+	sleep 1s
+	start_menu
+	;;
+    esac
+}
+
+start_menu
