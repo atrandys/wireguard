1aa88a575c41e0e66b3eaef6fd97d8a2456157ab
atrandys <37266675+atrandys@users.noreply.github.com>
2020-05-21 14:13:51 +0800

    Update wireguard_game.sh

--- wireguard_game.sh
+++ wireguard_game.sh
@@ -205,9 +205,7 @@ start_menu(){
     echo -e "\033[43;42m ====================================\033[0m"
     echo -e "\033[43;42m 介绍：wireguard+udpspeeder+udp2raw  \033[0m"
     echo -e "\033[43;42m 系统：CentOS7                       \033[0m"
-    echo -e "\033[43;42m 作者：atrandys                      \033[0m"
-    echo -e "\033[43;42m 网站：www.atrandys.com              \033[0m"
-    echo -e "\033[43;42m Youtube：atrandys                   \033[0m"
+    echo -e "\033[43;42m 作者：A                      \033[0m"
     echo -e "\033[43;42m ====================================\033[0m"
     echo
     echo -e "\033[0;33m 1. 升级系统内核(必需)\033[0m"
