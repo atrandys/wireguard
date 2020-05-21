5b54204e244ce0815381fe3af7627ce8d3f305a4
atrandys <37266675+atrandys@users.noreply.github.com>
2020-05-21 14:13:34 +0800

    Update wireguard_game_ubuntu.sh

--- wireguard_game_ubuntu.sh
+++ wireguard_game_ubuntu.sh
@@ -204,9 +204,7 @@ start_menu(){
     echo -e "\033[43;42m ====================================\033[0m"
     echo -e "\033[43;42m 介绍：wireguard+udpspeeder+udp2raw  \033[0m"
     echo -e "\033[43;42m 系统：Ubuntu                     \033[0m"
-    echo -e "\033[43;42m 作者：atrandys                      \033[0m"
-    echo -e "\033[43;42m 网站：www.atrandys.com              \033[0m"
-    echo -e "\033[43;42m Youtube：atrandys                   \033[0m"
+    echo -e "\033[43;42m 作者：A                     \033[0m"
     echo -e "\033[43;42m ====================================\033[0m"
     echo
     echo -e "\033[0;33m 1. 安装wireguard+udpspeeder+udp2raw\033[0m"
