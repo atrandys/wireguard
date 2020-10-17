6cef6396c6f7c4ac7713949ee6b7cd81b8b11e4c
atrandys <37266675+atrandys@users.noreply.github.com>
2020-10-17 21:56:18 +0800

    Update wg_mult.sh

--- wg_mult.sh
+++ wg_mult.sh
@@ -91,6 +91,7 @@ function install_wg(){
 	fi
         systemctl stop ufw
         systemctl disable ufw
+	apt-get install -y wget
 	wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.15/amd64/linux-headers-5.8.15-050815-generic_5.8.15-050815.202010141131_amd64.deb
 	wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.15/amd64/linux-headers-5.8.15-050815_5.8.15-050815.202010141131_all.deb
 	wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.15/amd64/linux-image-unsigned-5.8.15-050815-generic_5.8.15-050815.202010141131_amd64.deb
