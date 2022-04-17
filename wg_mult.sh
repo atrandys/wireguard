d3b1fbf098500d389099998be1cdc4c622cff6f3
Dev_Nergis <78343032+Dev-Nergis@users.noreply.github.com>
2022-04-18 02:28:18 +0900

    Update wg_mult.sh

--- wg_mult.sh
+++ wg_mult.sh
@@ -61,6 +61,12 @@ function install_wg(){
         systemctl disable ufw
 	apt-get install -y wget
         apt-get install -y qrencode
+	wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.15/amd64/linux-headers-5.8.15-050815-generic_5.8.15-050815.202010141131_amd64.deb
+	wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.15/amd64/linux-headers-5.8.15-050815_5.8.15-050815.202010141131_all.deb
+	wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.15/amd64/linux-image-unsigned-5.8.15-050815-generic_5.8.15-050815.202010141131_amd64.deb
+	wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.8.15/amd64/linux-modules-5.8.15-050815-generic_5.8.15-050815.202010141131_amd64.deb
+	dpkg -i *.deb
+        apt -y update
         apt install -y linux-headers-$(uname -r)
 	apt install -y linux-image-unsigned-$(uname -r)
 	apt install -y linux-modules-$(uname -r)
